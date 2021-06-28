7a287d98ba18c7509c232f0655ff9ccd9b8d6247
Pilar Dubiau <82668074+pilardubiau@users.noreply.github.com>
2021-06-28 17:31:55 -0300

    configuracion redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App'
+import App from './containers/App';
+import "./index.css"
+import {Provider} from "react-redux"
+import store from "./store/store"
 
 ReactDOM.render(
   <React.StrictMode>
+     <Provider>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
