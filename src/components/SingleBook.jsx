2211d5c55e3bf8882ffd0074f6583fb1f8bb6def
Alejandro Rusinoff <alejandrorusinoff@gmail.com>
2021-07-01 18:59:26 -0300

    casi terminado

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,21 +1,49 @@
 import React from "react";
 import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
+import {useHistory} from 'react-router-dom';
+import axios from 'axios'
+import '../styles/SingleBook.css'
 
 const SingleBook = ({singleBook}) => {
-
+  const history = useHistory()
+  const books = useSelector((state) => state.books)
+  const user = useSelector((state) => state.user)
+//userid viene del store y el bookid viene como parametro del map de abajo
+      const addOrder = (bookId) => {
+      const token = JSON.parse(localStorage.getItem('token'))
+      if(!user.id) {
+          history.push("/register")
+      }
+      else {
+          axios({
+              method: 'post',
+              url: '/api/orders',
+              data: {
+                userId: user.id,
+                bookId: bookId
+              },
+              headers: { authorization: `Bearer ${token}` },
+          }).then(()=> alert("Producto agregado al carrito"))
+      }
+  }
 
   return (
-    <div className="">
-      <div><img src={singleBook.img} alt="" /></div>
-            <h4>Title: {singleBook.title}</h4>
-            <br />
-            <br />
-            <h6>Actors: {singleBook.author}</h6>
-            <h6>Genre: {singleBook.category}</h6>
-            <div><p>Description: {singleBook.description}</p></div>
-            <div>Price: {singleBook.price} $$$</div>            
-        <div><button>Add to cart</button></div>
+    <div className="singlebook">
+        <h3>{singleBook.title}</h3>
+        <hr />
+        <div className="dataimg">
+          <div className="data">
+            <h5>Author: {singleBook.author}</h5>
+            <h5>Genre: {singleBook.category}</h5>
+            <h5>Publisher: {singleBook.publisher}</h5>
+            <h5>Rating: {singleBook.rating}</h5>
+            <h5>Price: U$D {singleBook.price}</h5>   
+          </div>
+          <img src={singleBook.img} alt="" />
+        </div>
+        <br />
+        <div><p>{singleBook.description}</p></div>            
+        <div className="boton"><button onClick={()=>addOrder(singleBook.id)}>Add to cart</button></div>
     </div>
   );
 };
--- src/containers/Carousel.jsx
+++ src/containers/Carousel.jsx
@@ -24,8 +24,16 @@ const Carousel1 = () => {
         Discover the very latest titles from the worlds of fiction, non-fiction and children’s. These great volumes are all out now.
           <div className="slideImagediv">
             {" "}
-            <img src={rndBooks[0]? rndBooks[0].img : null} class="d-block w-50" alt="..." />
-            <img src={rndBooks[1]? rndBooks[1].img : null} class="d-block w-50" alt="..." />
+            <img src={rndBooks[0]? rndBooks[0].img : null} /* class="d-block w-50"  */alt="..." />
+            <img src={rndBooks[1]? rndBooks[1].img : null} /* class="d-block w-50"  */alt="..." />
+            <img src={rndBooks[2]? rndBooks[2].img : null} /* class="d-block w-50" */ alt="..." />
+            <img src={rndBooks[3]? rndBooks[3].img : null} /* class="d-block w-50"  */alt="..." />
+            <img src={rndBooks[4]? rndBooks[4].img : null} /* class="d-block w-50" */ alt="..." />
+            <img src={rndBooks[5]? rndBooks[5].img : null} /* class="d-block w-50"  */alt="..." />
+            <img src={rndBooks[6]? rndBooks[6].img : null} /* class="d-block w-50" */ alt="..." />
+            <img src={rndBooks[7]? rndBooks[7].img : null} /* class="d-block w-50"  */alt="..." />
+            <img src={rndBooks[8]? rndBooks[8].img : null} /* class="d-block w-50" */ alt="..." />
+            
           </div>
           <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
@@ -33,8 +41,15 @@ const Carousel1 = () => {
       <Carousel.Item>
         <div className="slideImagediv">
           {" "}
-          <img src={rndBooks[2]? rndBooks[2].img : null} class="d-block w-50" alt="..." />
-          <img src={rndBooks[3]? rndBooks[3].img : null} class="d-block w-50" alt="..." />
+          <img src={rndBooks[2]? rndBooks[9].img : null} /* class="d-block w-50" */ alt="..." />
+          <img src={rndBooks[3]? rndBooks[10].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[2]? rndBooks[11].img : null} /* class="d-block w-50" */ alt="..." />
+          <img src={rndBooks[3]? rndBooks[12].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[2]? rndBooks[13].img : null} /* class="d-block w-50" */ alt="..." />
+          <img src={rndBooks[3]? rndBooks[14].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[2]? rndBooks[15].img : null} /* class="d-block w-50" */ alt="..." />
+          <img src={rndBooks[3]? rndBooks[16].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[2]? rndBooks[17].img : null} /* class="d-block w-50" */ alt="..." />
         </div>
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
@@ -42,8 +57,15 @@ const Carousel1 = () => {
       <Carousel.Item>
         <div className="slideImagediv">
           {" "}
-          <img src={rndBooks[4]? rndBooks[4].img : null} class="d-block w-50" alt="..." />
-          <img src={rndBooks[5]? rndBooks[5].img : null} class="d-block w-50" alt="..." />
+          <img src={rndBooks[4]? rndBooks[18].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[5]? rndBooks[19].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[2]? rndBooks[20].img : null} /* class="d-block w-50" */ alt="..." />
+          <img src={rndBooks[3]? rndBooks[21].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[2]? rndBooks[22].img : null} /* class="d-block w-50" */ alt="..." />
+          <img src={rndBooks[3]? rndBooks[23].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[2]? rndBooks[24].img : null} /* class="d-block w-50" */ alt="..." />
+          <img src={rndBooks[3]? rndBooks[25].img : null} /* class="d-block w-50"  */alt="..." />
+          <img src={rndBooks[2]? rndBooks[26].img : null} /* class="d-block w-50" */ alt="..." />
         </div>
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
