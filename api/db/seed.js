33b8e7bda834f795fe9c94b542663be07938c05a
ndmancini <ndmancini@gmail.com>
2021-07-05 22:52:06 -0300

    seed con más usuarios

--- api/db/seed.js
+++ api/db/seed.js
@@ -962,14 +962,43 @@ const seedBooks = [
     },
   ];
 
-  const seedAdmin = {
-    name: "Martin",
-    lastname: "Elmascapo",
-    address: "calle falsa 123",
-    username: "martinelmascapo",
-    email: "martin@martin.com",
-    password: "Hola123123",
-    isAdmin: true
-  }
+  const seedAdmin = [
+    {
+      name: "Martin",
+      lastname: "Elmascapo",
+      address: "calle falsa 123",
+      username: "martinelmascapo",
+      email: "martin@martin.com",
+      password: "Hola123123",
+      isAdmin: true
+    },
+    {
+      name: "Juan Domingo",
+      lastname: "Perón",
+      address: "San Vicente 123",
+      username: "juan_domingo",
+      email: "el_primer_trabajador@mail.com",
+      password: "Hola123123",
+    },
+    {
+      name: "Diego Armando",
+      lastname: "Maradona",
+      address: "Bella Vista 123",
+      username: "diegote",
+      email: "el_diego@mail.com",
+      password: "Hola123123",
+    },
+    {
+      name: "Señor",
+      lastname: "Analfabeto",
+      address: "Muy lejos 123",
+      username: "NoSabeLeer",
+      email: "queseyo@mail.com",
+      password: "Hola123123",
+    },
+  
+  ]
+
+  
   
   module.exports = {seedBooks, seedAdmin};
