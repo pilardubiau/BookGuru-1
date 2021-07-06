9cede9bf81bd31c58303f9d94a5149401494974e
matiascarou <matiascarou93@gmail.com>
2021-07-06 01:09:02 -0300

    fin del dia

--- api/db/seed.js
+++ api/db/seed.js
@@ -967,7 +967,7 @@ const seedBooks = [
       name: "Martin",
       lastname: "Elmascapo",
       address: "calle falsa 123",
-      username: "martinlibrosky",
+      username: "martinlibrolover",
       email: "martin@martin.com",
       password: "Hola123123",
       isAdmin: true
